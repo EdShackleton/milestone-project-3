6b78185994483b60585f4757ed29aedf8d18689e
Ubuntu <ubuntu@ip-172-31-82-238.ec2.internal>
2019-06-12 13:15:25 +0000

    added login system

--- app.py
+++ app.py
@@ -9,6 +9,55 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:Welcome3@myfirstcluster-8h2g0.mong
 
 mongo = PyMongo(app)
 
+#login system
+
+@app.route('/go_to_login')
+def go_to_login():
+    return render_template('login.html')
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Your are logged in as ' + session['username']
+    
+    return render_template(login.html)
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+    
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode['utf-8'], login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+            
+        return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user.find_one({'name' : request.form['username']})
+        
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'], bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+    
+    return render_template('register.html')
+        
+        
+    return ''
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(debug=True)
+
+#CRUD
 
 @app.route('/')
 @app.route('/get_jams')
